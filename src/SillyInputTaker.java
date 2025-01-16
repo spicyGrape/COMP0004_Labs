d39233376de7eabead18a32cdfcd09eb7d68512e
Shuaiting Li <li.private0@gmail.com>
2025-01-16 12:26:54 +0000

    add SillyInputTaker

---
+++ src/SillyInputTaker.java
@@ -0,0 +1,20 @@
+// Written by Shuaiting Li, January 2025
+// Solution to LabSheet 1, Q1.1.
+// A program that inputs a sequence of Strings until the word stop is entered.
+public class SillyInputTaker {
+    //Take the input and check whether the input is "stop"
+    public void takeInput() {
+        Input in = new Input();
+        do {
+            System.out.println("Please enter a String: ");
+            String line = in.nextLine();
+            if (line.compareTo("stop") == 0) {
+                break;
+            }
+        } while (true);
+    }
+
+    public static void main(String[] args) {
+        new SillyInputTaker().takeInput();
+    }
+}
