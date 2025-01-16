4206560f055919e454e729ff9700c124472b124a
Shuaiting Li <li.private0@gmail.com>
2025-01-16 13:32:37 +0000

    add SquareRootOfSum

---
+++ src/BaseTranslator.java
@@ -0,0 +1,13 @@
+// Written by Shuaiting Li, January 2025
+// Solution to lab sheet 1 Q1.7
+// To translate number into various bases.
+public class BaseTranslator {
+    public String toBase(int n, int base) {
+        return Integer.toString(n, base);
+    }
+
+    public static void main(String[] args) {
+        Input in = new Input();
+        System.out.println(new BaseTranslator().toBase(in.nextInt(), in.nextInt()));
+    }
+}
--- src/SquareRootOfSum.java
+++ src/SquareRootOfSum.java
@@ -2,6 +2,15 @@
 // Solution to lab sheet 1 Q1.5
 // To take two doubles and return the square root of the sum of them.
 public class SquareRootOfSum {
+
+    private double a;
+    private double b;
+
+    private void getAB() {
+        this.a = this.getDouble();
+        this.b = this.getDouble();
+    }
+
     private double getDouble() {
         System.out.print("Please enter a number: ");
         return new Input().nextDouble();
@@ -13,7 +22,8 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         SquareRootOfSum sq = new SquareRootOfSum();
-        double ans = sq.squareRootOfSum(sq.getDouble(), sq.getDouble());
+        sq.getAB();
+        double ans = sq.squareRootOfSum(sq.a, sq.b);
         System.out.println("The square root of the numbers are: " + ans);
     }
 }
