8764fdd98880e0d919de50342bbc3215c460112b
Shuaiting Li <li.private0@gmail.com>
2025-01-19 13:36:04 +0000

    add StringPalindrome

---
+++ src/StringPalindrome.java
@@ -0,0 +1,24 @@
+// Written by Shuaiting Li, January 2025
+// Solution to Lab Sheet 1 Q1.10.
+// A program to remove all whitespaces and punctuations in a string, then decided whether it is a palindrome.
+public class StringPalindrome {
+    public boolean is_palindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        StringPalindrome sp = new StringPalindrome();
+        System.out.print("Please enter a String: ");
+        System.out.println(sp.is_palindrome(new Input().nextLine()));
+    }
+}
