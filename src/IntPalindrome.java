21944d094731ea3fbba9c5c71500afc123072ce0
Shuaiting Li <li.private0@gmail.com>
2025-01-19 13:19:30 +0000

    add IntPalindrome

---
+++ src/IntPalindrome.java
@@ -0,0 +1,20 @@
+// Written by Shuaiting Li, January 2025
+// Solution to lab sheet 1 Q1.9.
+// A program to determine whether a long int is a palindrome
+public class IntPalindrome {
+    public boolean is_palindrome(long n) {
+        long n_copy = n;
+        long reversed = 0;
+        while (n != 0) {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
+        }
+        return reversed == n_copy;
+    }
+
+    public static void main(String[] args) {
+        IntPalindrome ip = new IntPalindrome();
+        System.out.print("Please enter a long int: ");
+        System.out.println(ip.is_palindrome(new Input().nextLong()));
+    }
+}
